59d5d52c71c78aa5676f2780091707cb3e5f3119
bartkrekelberg <bart@vision.rutgers.edu>
2023-10-01 03:01:44 -0400

    .

--- +sbx/Eye.m
+++ +sbx/Eye.m
@@ -10,8 +10,8 @@ quality : longblob # The quality of the estimation at each time point [nrTimePoi
 manualqc = NULL : smallint # quantify the overall quality based on manual inspection. 
 nrtimepoints :  int unsigned # Number of time points in the pose estimation
 width = NULL : float # Width of the camera image
-height =NULL  : float # Height of the camera image
-framerate =NULL : float # Framerate of the movie
+height = NULL  : float # Height of the camera image
+framerate = NULL : float # Framerate of the movie
 %}
 %
 % BK - Sept 2023.
@@ -119,6 +119,7 @@ classdef Eye < dj.Computed
                         % DLC to allow DLC model comparisons.
                         mvFile =  sbx.Eye.movieFile(key);
                         [x,y,a,quality,nrT] = sbx.Eye.dlc(mvFile, parms);
+                        w=NaN;h=NaN;fr=NaN;
                     else
                         error('Unknown %d tag',key.tag);
                     end
@@ -255,11 +256,11 @@ classdef Eye < dj.Computed
 
             mvFile = strrep(mvFile,'\','/');
             [videoFolder,videoFile,videoType]= fileparts(mvFile);
-            csvFile = fileparts(videoFolder,[videoFile + parms.suffix ".csv"]);
+            csvFile = fullfile(videoFolder,[videoFile + parms.suffix + ".csv"]);
 
             if exist(csvFile,"File")
                 % Skip running DLC
-                fprintf('DLC output (%s) already exists. Adding to the table.',csvFile);
+                fprintf('DLC output (%s) already exists. Adding to the table.\n',csvFile);
             else
 
                 %% Construct the Python command
