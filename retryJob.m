1e71375465208e127bee62ab5a1eb98920899443
bartkrekelberg <bart@vision.rutgers.edu>
2024-02-02 16:27:28 +0100

    Added retryJob
    fetchtable always string

---
+++ retryJob.m
@@ -0,0 +1,42 @@
+function T= retryJob(row)
+% Retry populating an item in the Jobs table that caused an error. This
+% retry will put a breakpoint on the line where the previous attempt
+% failed and stop execution there. 
+% 
+% If this funciton runs to completion, and is successful, the job will be
+% removed from the jobs table.
+% 
+% Returns the subset of the Jobs table with the same error message (and
+% table_name). If the problem is fixed, the user can call del on this
+% returen table to clear those errors.
+arguments
+    row  = 1  % The row of the Jobs table to process
+end
+
+tpl = fetch(ns.Jobs ,'*',sprintf('LIMIT 1 OFFSET %d',row-1));
+
+%% Setup the break point
+[p,f] =fileparts(tpl.error_stack(1).file);
+[~,nmspc] =fileparts(p);
+if contains(nmspc,'+')
+    fname = [nmspc(2:end) '.' f];
+else
+    fname =f;
+end
+dbstop('in', string(fname),'at', string(tpl.error_stack(1).line));
+
+%% Call populate
+try
+    populate(feval(tpl.table_name),tpl.key);
+    success=true;
+catch
+    me.message
+    success=false;    
+end
+
+%% Success - remove the item from the jobs table
+if success
+    delQuick(ns.Jobs & ns.stripToPrimary(ns.Jobs,tpl))
+end
+
+T = ns.Jobs & struct('table_name',tpl.table_name,'error_message',tpl.error_message);
