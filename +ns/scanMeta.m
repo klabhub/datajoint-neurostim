7bda0328ac75b84bd0f1345eb1646630f28b2ecb
bartkrekelberg <bart@vision.rutgers.edu>
2024-05-16 11:55:09 +0200

    Wrapped with a transaction

--- +ns/scanMeta.m
+++ +ns/scanMeta.m
@@ -1,7 +1,7 @@
 function scanMeta(tbl,pv)
 % Scan the file tree for JSON files with meta data and update the datajoint
 % database with the new values.
-% 
+%
 % Typically these data will have been entered in the nsMeta app (by hand),
 % this funciton synchronizes the state of the JSON files in the file tree
 % with the tables in datajoint. Because this only runs over
@@ -11,7 +11,7 @@ function scanMeta(tbl,pv)
 % INPUT
 % tbl  -  A subject,session, or experiment table
 arguments
-    tbl (1,1) {mustBeA(tbl,{'ns.Experiment','ns.Subject','ns.Session'})}   
+    tbl (1,1) {mustBeA(tbl,{'ns.Experiment','ns.Subject','ns.Session'})}
     pv.dryrun (1,1) logical = false
 end
 % Get the meta table
@@ -23,57 +23,65 @@ if strcmpi(className ,'ns.Subject')
     jsonFile  =fullfile(getenv("NS_ROOT"),'subject.json');
     allJson = readJson(jsonFile);
 end
-
-if pv.dryrun
-    fprintf('DRY RUN - NO CHANGES WILL BE MADE')
-else
-    % Delete the old meta table - everything is stored in JSON which will
-    % be read below
-    delQuick(metaTbl);
-end
-% Loop over the table to read the json files and construct an array of tpls
-for key  = fetch(tbl)'
-    switch (className)
-        case 'ns.Subject'
-            thisJson = allJson([allJson.subject]==key.subject);
-            if ~isempty(thisJson)
-                thisJson = rmfield(thisJson,'subject');
-            end
-        case 'ns.Session'
-            jsonFile  =fullfile(folder(ns.Session&key),[key.subject '.json']);
-            thisJson = readIfExists(jsonFile);            
-        case 'ns.Experiment'
-            matFile  =file(ns.Experiment & key);
-            jsonFile= regexprep(matFile,'(\.mat$)','.json'); % Swap extension
-            thisJson = readIfExists(jsonFile);           
+dj.conn().startTransaction
+try
+    if pv.dryrun
+        fprintf('DRY RUN - NO CHANGES WILL BE MADE\n')
+    else
+        % Delete the old meta table - everything is stored in JSON which will
+        % be read below
+        delQuick(metaTbl);
     end
-    if ~isempty(thisJson)                      
-        fn=fieldnames(thisJson);   
-        newTpl = mergestruct(key,struct('meta_name',fn,...
-                                     'meta_value',struct2cell(thisJson)));
-        if exist('tpl',"var")
-            tpl = catstruct(1,tpl,newTpl);
-        else 
-            tpl = newTpl;
+    % Loop over the table to read the json files and construct an array of tpls
+    for key  = fetch(tbl)'
+        switch (className)
+            case 'ns.Subject'
+                thisJson = allJson([allJson.subject]==key.subject);
+                if isempty(thisJson)
+                    fprintf("No metadata found in %s for %s \n ",jsonFile, key.subject)
+                else
+                    thisJson = rmfield(thisJson,'subject');
+                end
+            case 'ns.Session'
+                jsonFile  =fullfile(folder(ns.Session&key),[key.subject '.json']);
+                thisJson = readIfExists(jsonFile);
+            case 'ns.Experiment'
+                matFile  =file(ns.Experiment & key);
+                jsonFile= regexprep(matFile,'(\.mat$)','.json'); % Swap extension
+                thisJson = readIfExists(jsonFile);
+        end
+        if ~isempty(thisJson)
+            fn=fieldnames(thisJson);
+            newTpl = mergestruct(key,struct('meta_name',fn,...
+                'meta_value',struct2cell(thisJson)));
+            if exist('tpl',"var")
+                tpl = catstruct(1,tpl,newTpl);
+            else
+                tpl = newTpl;
+            end
         end        
     end
-end
-% Add the found tpls to the data base.
-if exist('tpl',"var") 
-    fprintf('Inserting %d meta data.\n' ,numel(tpl))
-    if pv.dryrun
-         fprintf('DRY RUN - NO CHANGES MADE')
+    % Add the found tpls to the data base.
+    if exist('tpl',"var")
+        fprintf('Inserting %d meta data.\n' ,numel(tpl))
+        if pv.dryrun
+            fprintf('DRY RUN - NO CHANGES MADE.\n')
+        else
+            % In with the new
+            insert(metaTbl,tpl)
+        end
     else
-        % In with the new
-        insert(metaTbl,tpl)        
+        fprintf('No relevant meta data found. \n')
     end
-else
-    fprintf('No relevant meta data found.')
+catch me
+    fprintf(2,'An error occurred. Cancelling the transaction. No changes have been made. (%s)\n',me.message)
+    dj.conn().cancelTransaction
+    return;
+end
+dj.conn().commitTransaction
 end
 
-end
-
-
+%% Helper
 function thisJson = readIfExists(f)
 if exist(f,"file")
     thisJson = readJson(f);
