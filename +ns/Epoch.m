f84cf600c17c442e92b462ab969213e4f257ddab
Mert Özkan <mert.ozkan@rutgers.edu>
2025-03-18 07:41:54 -0400

    Epoch transform, plot, get_trial_numbers

--- +ns/Epoch.m
+++ +ns/Epoch.m
@@ -178,7 +178,7 @@ classdef Epoch < dj.Computed
 
             % In the future add another method called get_epochs with
             % averaging options, basically it should include the code around after
-            % '%fetch data' 
+            % '%fetch data'
 
             arguments
 
@@ -186,10 +186,9 @@ classdef Epoch < dj.Computed
 
                 pv.channel = [] % if empty all channels
                 pv.trials = [] % if empty, all trials
-                pv.average = false  % if true,
-                pv.collapse = 'trials' % can be 'channels', 'trials', 'all'
+                pv.average = 'trials' % can be 'channels', 'trials', 'all'
 
-                pv.across = 'time' % can be 'frequency'
+                pv.across = 'time' % can be 'frequency' or 'time-frequency'
                 pv.type = 'line' % can be 'raster'
 
                 pv.time_window = [];
@@ -201,174 +200,261 @@ classdef Epoch < dj.Computed
 
             end
 
-            % Decide on figures and subplotting
-            primary_keys = fetchtable(eTbl);
+            assert(ismember(pv.average, {'channels', 'trials', 'all'}), ...
+                "ns.Epoch can only plot averages across 'channels', 'trials', or 'all'.");
 
-            ctags = unique(primary_keys.ctag);
-            n_ctag = length(ctags);
-            
-            etags = unique(primary_keys.etag);
-            n_etag = length(etags);
+            [datN, t] = retrieve(eTbl, "channel", pv.channel, "trials", pv.trials, "time_window", pv.time_window);
+            datN = ns.Epoch.transform(datN, "average", pv.average);
 
-            subs = unique(primary_keys.subject);
-            n_sub = length(subs);  % each subject will have a different figure
+            separate_figs_by = intersect(lower(datN.Properties.VariableNames), ["subject", "etag", "ctag", "dimension"]);
+            n_figs_by_dim = varfun(@(x) length(unique(x)), datN(:,separate_figs_by), 'OutputFormat','uniform');
+            n_figs = prod(n_figs_by_dim);
 
-            dims = unique(primary_keys.dimension);
-            n_dim = length(dims); % each dimension will have a different figure
+            n_subplot = length(unique(datN.name));
 
-            conds = unique(primary_keys.name);
-            n_cond = length(conds); % each level will be ploted at a different row in the same figure
+            for iFig = 1:n_figs
 
-            [~, dt] = sampleTime(ns.C & eTbl);
-            epoch_win = getfield(getfield(fetch(ns.EpochParameter & eTbl,'pv'),'pv'),'epoch_win');
-            t = epoch_win(1):dt:epoch_win(2);
+                figure;
+                tileN = tiledlayout(n_subplot, 1, 'TileSpacing', 'none', 'Padding', 'none');
 
-            for iC = 1:n_ctag
+                for iSp = 1:n_subplot
 
-                cN = ctags{iC};
-
-                for iE = 1:n_etag
-
-                    eN = etags{iE};
-                
-                    for iSub = 1:n_sub
-
-                        subN = subs{iSub};
-
-                        for iDim = 1:n_dim
-
-                            dimN = dims{iDim};
-                            iFig = sub2ind([n_ctag, n_etag, n_sub,n_dim], iC, iE, iSub, iDim); % figure number
-
-                            % Conditions are subplotted
-                            for iCond = 1:n_cond
-
-                                condN = conds{iCond};
-                                % fetch data
-
-                                dat_qry = ns.Epoch.query_restricter( ...
-                                    'ctag', cN, 'etag', eN, 'subject', subN, 'dimension', dimN, 'name', condN, ...
-                                    'channel', pv.channel);
-                                dat = fetch(ns.EpochChannel & eTbl & dat_qry, 'signal');
-                                
-                                % Select trials to plot                                
-                                isValid = ~isinf(getfield(fetch(ns.Epoch & dat_qry,'event_onset'),'event_onset')); % inf indicates that the trial was not shown
-                                trials = getfield(fetch(ns.DimensionCondition & eTbl & dat_qry, 'trials'), 'trials'); % the actual trial numbers
-
-                                if ~isempty(pv.trials)
-
-                                    trials2plot = intersect(pv.trials, trials(isValid));
-                                    isPlotTrials = ismember(trials(isValid),trials2plot);
-
-                                else
-
-                                    isPlotTrials = ones(1,sum(isValid));
-
-                                end
-
-                                dat = cat(3, dat(:).signal); % trial by timepoint by channel
-                                dat = permute(dat,[1, 3, 2]); % trial by channel by timepoint
-                                dat = dat(isPlotTrials,:,:);
-
-                                % Collapse
-                                switch pv.collapse
-
-                                    case 'channels'
-
-                                        dat = squeeze(mean(dat,2));
-
-                                    case 'trials'
-
-                                        dat = squeeze(mean(dat,1));
-
-                                    case 'all'
-
-                                        dat = squeeze(mean(dat,[1,2]))';
-
-                                    otherwise
-
-                                        error("Only collapsing options are 'channels', 'trials', 'all'");
-
-                                end
-                                
-                                % plots across time or frequency, the latter req
-                                % uires more options
-
-                                figure(iFig);
-                                subplot(n_cond, 1, iCond);
-                                switch pv.across
-
-                                    case 'time'
-
-                                        if isempty(pv.time_window)
-
-                                            x = t;
-                                            isInEpoch = ones(size(t));
-                                        else
-
-                                            isInEpoch = t>= pv.time_window(1) & t<=pv.time_window(2);
-                                            x = t(isInEpoch);
-                                        end
-                                            x_unit = "ms";
-                                            y_unit = "\muV";
-                                            
-                                    case 'frequency'
-
-                                    otherwise
-
-                                        error('Only can plot across time or frequency');
-
-                                end
-
-                                switch pv.type
-
-                                    case 'line'
-
-                                    case 'raster'
-
-                                end
-
-                                
-
-                            end
-    
+                    axN = nexttile(tileN);
+                    if ~isempty(datN.signal{iFig})
+                        n_rows = size(datN.signal{iFig}, 1);
+                        for j = 1:n_rows
+                            plot(ax, t, datN.signal{iFig}(j, :), 'LineWidth', 1.5);
+                            hold(ax, 'on');
                         end
+                        hold(ax, 'off');
+                    else
+                        plot(ax, NaN, NaN);
                     end
-                    
+
+
                 end
+
 
             end
 
 
-
-        %%
-
+            %%
+            
         end
+
+        function trials = get_trial_numbers(eTbl)
+
+                isValid = fetch(eTbl,'event_onset'); % inf indicates that the trial was not shown
+                n_key = length(isValid);
+                trials = fetchtable(eTbl);
+                for i = 1:n_key
+
+                    trlN = fetch(ns.DimensionCondition & isValid(i), 'trials'); % the actual trial numbers
+                    trials.trial{i} = [trlN.trials(~isinf(isValid(i).event_onset))];
+
+                end
+
+            end
 
     end
 
 
     methods (Static, Access = public)
 
-        function s = query_restricter(varargin)
+        function op = transform(op, varargin)
 
-            if nargin==0 || rem(nargin,2)
-                error("restricter takes in an even number of inputs such that \n \t restricter (field1, value1, ...).")
-            end
+            iOper = 1;
 
-            n_fields = nargin/2;
+            while iOper <= nargin-1
 
-            for iField = 1:2:nargin
+                argN = varargin{iOper};
+                iOper = iOper + 1;
+                switch lower(argN)
+                    
+                    case "average"
 
-                if isempty(varargin{iField+1})
+                        subargN = varargin{iOper};
+                        iOper = iOper + 1;
+                        switch lower(subargN)
 
-                    varargin(iField:(iField+1)) = [];
+                            case 'trials'
 
+                                avg_dim = 1;
+
+                            case 'channels'
+
+                                avg_dim = 2;
+
+                            case 'all'
+
+                                avg_dim = [1,2];
+
+                        end
+
+                        op.signal = cellfun(@(x) ns.Epoch.make_row_if_column_(squeeze(mean(x, avg_dim))), op.signal, 'UniformOutput', false);
+
+                    case "pad"
+
+                        subargN = varargin{iOper};
+                        iOper = iOper + 1;
+
+                        switch lower(subargN)
+
+                            case {'nan', 'nans'}
+
+                                filler = NaN;
+
+                            case {'zero', 'zeros'}
+
+                                filler = 0;
+
+                            otherwise
+
+                                error('Epoch signal can only be padded with "zero" or "nan".');
+
+                        end
+
+                        end_length = varargin{iOper};
+                        iOper = iOper + 1;
+                        assert(isnumeric(end_length), 'Pad length must be numeric and given as the 3rd input following the input "pad".');
+                        op.signal = cellfun(@(x) paddata(x, end_length, Side='Both', FillValue=filler, Dimension=ndims(x)), ...
+                            op.signal, 'UniformOutput', false);
+
+                    case "window"
+                    case "fft"
+
+                        sfreq = varargin{iOper};
+                        iOper = iOper + 1;
+                        if ~isnumeric(sfreq) && strcmp(sfreq, "sfreq")
+                            sfreq = varargin{iOper};
+                            iOper = iOper + 1;
+                        end
+
+                        for i = 1:height(op)
+
+                            [op.amplitude{i}, op.phase{i}, op.frequency{i}] = do_fft(op.signal{i}, ndims(op.signal{i}), sfreq);
+
+                        end
+
+                    case "psd"
+
+                        sfreq = varargin{iOper};
+                        iOper = iOper + 1;
+                        if ~isnumeric(sfreq) && strcmp(sfreq, "sfreq")
+                            sfreq = varargin{iOper};
+                            iOper = iOper + 1;
+                        end
+
+                        if nargin > iOper
+                            opts = varargin{iOper};
+                            if strcmp(opts, "options")
+
+                                opts = varargin{iOper+1};
+                                iOper = iOper + 2;
+                            end
+
+                        else
+                            opts = {};
+                        end
+
+                        for i = 1:height(op)
+
+                            [op.power{i}, op.frequency{i}] = do_psd(op.signal{i}, sfreq, ndims(op.signal{i}), opts{:});
+
+                        end
+
+                    case "snr"
+
+                        by_var = varargin{iOper};                        
+                        if ~ismember(by_var, ["amplitude", "power"])
+                            by_var = "amplitude";
+                            if ~ismember(by_var, op.Properties.VariableNames)
+                                by_var = "power";
+                            end
+                        else
+                            iOper = iOper + 1;
+                        end
+
+                        subargN = varargin{iOper};
+                        iOper = iOper + 1;
+                        switch lower(subargN)
+                            case {"neighbors"}
+
+                                n_bins = varargin{iOper};
+                                iOper = iOper + 1;
+
+                                op.snr = cellfun(@(x) ...
+                                    gen.apply_func_along_dimension(x, ndims(x), @ns.Epoch.divide_by_n_neighbor_, n_bins), ...
+                                    op.(by_var), 'UniformOutput', false);
+
+                            case {"medfilt", "median"}
+
+                                n_bins = varargin{iOper};
+                                iOper = iOper + 1;
+
+                                op.snr = cellfun(@(x) ...
+                                    gen.apply_func_along_dimension(x, ndims(x), @ns.Epoch.divide_by_medfilt_, n_bins), ...
+                                    op.(by_var), 'UniformOutput', false);
+
+                            case {"average"}
+                            otherwise
+                        end
                 end
 
             end
 
-            s = struct(varargin{:});
+            function [pow, frequencies] = do_psd(dat, fs, iDim, varargin)
+
+                dim_size = size(dat);
+                dim_size(iDim) = 1;
+                slice_idx = arrayfun(@(x) 1:x, dim_size, 'UniformOutput',false);
+                slice_idx{iDim} = ':';
+                slice_idx = table2cell(combinations(slice_idx{:}));
+
+                [pow, frequencies] = gen.apply_func_along_dimension(dat, iDim, @pspectrum, fs, varargin{:});
+
+                select_idx = repmat({1},1, ndims(pow));
+                select_idx{iDim} = ':';
+                frequencies = squeeze(frequencies(select_idx{:}))';
+
+
+            end
+
+            function [amplitude, phase, frequencies] = do_fft(data, n, fs)
+                % fftSliceReal - Computes FFT amplitude and phase for each slice along the n'th dimension.
+                %                Only includes real frequencies.
+                %
+                % Inputs:
+                %   data: Input multi-dimensional data.
+                %   n: Dimension along which to slice and compute FFT.
+                %   fs: Sampling frequency (optional, default is 1).
+                %
+                % Outputs:
+                %   amplitude: Amplitude of the FFT.
+                %   phase: Phase of the FFT.
+                %   frequencies: Corresponding real frequencies.
+
+                n_dim = ndims(data);
+
+                % Compute FFT for each slice
+                fftResult = fft(data, [], n_dim);
+
+                idx = repmat({':'},1,n_dim);
+                % Calculate real frequencies
+                N = size(data, n);
+                if mod(N, 2) == 0
+                    frequencies = (0:N/2) * fs / N;
+                    idx{n_dim} = 1:N/2+1;
+                else
+                    frequencies = (0:(N-1)/2) * fs / N;
+                    idx{n_dim} = 1:(N+1)/2;
+                end
+
+                amplitude = abs(fftResult(idx{:}));
+                phase = angle(fftResult(idx{:}));
+
+
+            end
 
         end
 
@@ -377,8 +463,49 @@ classdef Epoch < dj.Computed
 
         function raster_plot()
         end
-       
 
+    end
+
+    methods (Static, Access = protected)
+
+        function arr = make_row_if_column_(arr)
+
+            if iscolumn(arr), arr = arr'; end
+        end
+
+        function arr = make_column_if_row_(arr)
+
+            if isrow(arr), arr = arr'; end
+        end
+
+        function denoised_vec = divide_by_n_neighbor_(vec, n)
+
+            len = length(vec);
+            noise_vec = zeros(size(vec));
+
+            for i = 1:len
+
+                if i <= n || i > len - n
+
+                    noise_vec(i) = NaN;
+
+                else
+
+                    noise_vec(i) = mean(vec([(i-n):(i-1), (i+1):(i+n)]));
+
+                end
+
+            end
+
+            denoised_vec = vec./noise_vec;
+
+        end
+
+        function denoised_vec = divide_by_medfilt_(vec, n)
+
+            denoised_vec = vec ./ medfilt1(vec, n, 'truncate');
+
+        end
     end
 
 end
