e86014e3602f1bcfa874a2102e91c133aa37555d
bartkrekelberg <bart@vision.rutgers.edu>
2025-03-27 12:52:15 +0100

    .

--- nsParPool.m
+++ nsParPool.m
@@ -1,9 +1,10 @@
-function pool = nsParPool()
-% Function to start a local parpool based on an environment variable. 
+function pool = nsParPool(pv)
+% Function to start (or not start) a local parpool based on an 
+% environment variable. 
 % 
-% This is useful to avoid storing things like nrWorkers with parameters for
-% a makeTuples function  (which forces the same number of workers forever
-% and is annoying when debugging).
+% This is useful to avoid storing or hardcoding things like nrWorkers 
+% in a table for a makeTuples function  (which forces the same number 
+% of workers forever and is annoying when debugging).
 % 
 % To use this, write parfor code like this
 % 
@@ -25,17 +26,26 @@ function pool = nsParPool()
 % To temporarily use the for-loop even with a parpool running, call
 % setenv("NS_PARFOR",0)
 %
-nrWorkers = str2double(getenv("NS_PARFOR"));
+% If this function creates the pool, SpmdEnabled is set to false by
+% default. Set the spmd parameter to true to enable it on the pool.
+%
+% Calling this function with the nrWorkers argument set bypassess the
+% environment variable (you can use this to start a pool manually for
+% instance, but using that in code defeats the purpose of the function).
+arguments
+    pv.nrWorkers {mustBePositive} = str2double(getenv("NS_PARFOR"));
+    pv.spmd = false
+end
 pool = gcp("nocreate");
-if isnan(nrWorkers)
+if isnan(pv.nrWorkers)
     % The environment variable was not set: use a pool only if it already 
     % exists     
-elseif nrWorkers==0
+elseif pv.nrWorkers==0
     % Explicitly disallow using a pool (but do not close it).
     pool = [];
 elseif isempty(pool)
     % create a pool with the requested number of workers
-    pool = parpool('Processes',nrWorkers);
+    pool = parpool('Processes',pv.nrWorkers,'SpmdEnabled',pv.spmd);
 else 
     % use the pool as is. 
 end       
