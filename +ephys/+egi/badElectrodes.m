931522b18cf7737eb6874ce8e476c4d6bfd2ad7b
bartkrekelberg <bart@vision.rutgers.edu>
2025-04-01 15:52:29 +0200

    Fixed artfiact table issues (failed on no channel artifacts; skipping those now)

--- +ephys/+egi/badElectrodes.m
+++ +ephys/+egi/badElectrodes.m
@@ -1,23 +1,23 @@
 function [perExpt,perChannel] = badElectrodes(C,parms)
-% Read file with a list of bad electrodes 
+% Read file with a list of bad electrodes
 % parms.filename = string that must be in the filename (typically  badElectrodes)
 % parms.extension = extension of the file (typically .xlsx)
-% 
+%
 % For instance, for subject 123 there will be a file called
 % 123.badElectrodes.xlsx in the session folder. It contains a list of
 % electrodes considered "bad".
-% See Also ns.Artifact 
+% See Also ns.Artifact
 arguments
     C (1,1) ns.C {mustHaveRows(C,1)}
     parms (1,1) struct
-end 
+end
 
 %% Find the relevant file
 expt =(ns.Experiment & C);
 file = (ns.File & expt) & struct('extension',parms.extension) & ['filename LIKE "%' char(parms.filename) '%"'];
 fldr = folder(expt);
 T=table;
-if exists(file) 
+if exists(file)
     fn = fetch1(file,'filename');
     ff= fullfile(fldr,fn);
     if exist(ff,'file')
@@ -28,16 +28,17 @@ if exists(file)
     else
         fprintf(2,'File (%s) not found in folder %s\n',fn,fldr)
     end
+    badElectrodes = T{:,1};
+    perChannel =struct('start',[],'stop',[],'trial',[],'channel',num2cell(badElectrodes));
 else
     fprintf('No %s for %s',parms.filename,fldr)
+    perChannel =struct('start',[],'stop',[],'trial',[],'channel',[]);
 end
 
 perExpt = struct('start',[],'stop',[],'trial',[]); % Nothing applicable to all channels
-if isempty(T)
-    perChannel =struct('start',[],'stop',[],'trial',[],'channel',[]);
-else
-    perChannel =struct('start',[],'stop',[],'trial',[],'channel',num2cell(T{:,1}));
-end   
+
+
+
 
 end
 
--- +ns/Artifact.m
+++ +ns/Artifact.m
@@ -2,7 +2,7 @@
 # Identifies trials and time periods that should not be used for analysis
 -> ns.C
 -> ns.ArtifactParm
---- 
+---
 trial = NULL : longblob   # A vector of trials that are considered artifacts
 start = NULL : longblob  # A vector of neurostim times that indicate the start of an artifact period
 stop  = NULL : longblob   # A vector of neurostim times that indicate the end of an artifact period
@@ -18,10 +18,10 @@ classdef Artifact < dj.Computed
     methods
         function v = get.keySource(~)
             % Restricted to ns.C tuples with the ctag specified in
-            % ArtifactParm 
-            allTpl = [];                       
+            % ArtifactParm
+            allTpl = [];
             for thisPrm= fetch(ns.ArtifactParm,'c','paradigm')'
-                tbl = (ns.C*ns.CParm) & struct('ctag',thisPrm.c);    
+                tbl = (ns.C*ns.CParm) & struct('ctag',thisPrm.c);
                 if ~isempty(thisPrm.paradigm)
                     tbl = tbl & (ns.Experiment & struct('paradigm',thisPrm.paradigm));
                 end
@@ -37,23 +37,22 @@ classdef Artifact < dj.Computed
             end
             % And then restrict the full table by the set of found tuples.
             v = (ns.C*proj(ns.ArtifactParm,'atag','fun','description','parms')) & allTpl;
-        end 
+        end
     end
     methods (Access=protected)
         function makeTuples(tbl,key)
             prms = fetch(ns.ArtifactParm &key,'*');
             assert(~isempty(which(prms.fun)),'Artifact detection function %s not found.',prms.fun);
-            [experimentTpl,channelTpl] = feval(prms.fun,ns.C&key,prms.parms); % Pass to handler            
-            % Setup the artifact table (applies to all channels)            
+            [experimentTpl,channelTpl] = feval(prms.fun,ns.C&key,prms.parms); % Pass to handler
+            % Setup the artifact table (applies to all channels)
             aKey = mergestruct(key,experimentTpl);
             insert(tbl,aKey);
-
-            if ~(isempty(channelTpl) || isempty([channelTpl.channel]))
+            if ~isempty(channelTpl) && ~isempty(channelTpl.channel)
                 % Add channel specific artifact info
                 acKey = mergestruct(key,channelTpl);
                 insert(ns.ArtifactChannel,acKey);
             end
         end
-     end
+    end
 
 end
