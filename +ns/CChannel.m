943fca7a4d816c685cc98643f2dd73bfc73bdea0
bartkrekelberg <bart@vision.rutgers.edu>
2025-09-29 03:42:58 -0400

    .

--- +ns/CChannel.m
+++ +ns/CChannel.m
@@ -5,12 +5,12 @@ channel : int # The channel that recorded the signal
 ---
 signal : longblob # The preprocessed signal for each sample in the experiment
 name = NULL : varchar(128) # A name for this channel 
-mean = NULL : float % Mean of the signal
-median = NULL :  float % Median of the signal
-stdev = NULL : float % Stddev of the signal
-min  = NULL :  float  % Minimum of the signal
-max = NULL :  float   % Maximum of the signal
-nan = NULL : float   % Fraction of nans
+mean = NULL : float # Mean of the signal
+median = NULL :  float # Median of the signal
+stdev = NULL : float # Stddev of the signal
+min  = NULL :  float  # Minimum of the signal
+max = NULL :  float   # Maximum of the signal
+nan = NULL : float   # Fraction of nans
 channelinfo = NULL : longblob # Information on this preprocessed channel
 %}
 %
