b2c6e02e5ce3c020ef3a285deaca25609a185042
Mert Özkan <mert.ozkan@rutgers.edu>
2025-10-12 12:36:41 -0400

    Update detect_bad_behavior_epochs.m
    bug fix

--- +ns/detect_bad_behavior_epochs.m
+++ +ns/detect_bad_behavior_epochs.m
@@ -3,6 +3,7 @@ function varargout = detect_bad_behavior_epochs(varargin)
 warning(['Deprecation Warning!!\n\t ns.detect_bad_behavior_epochs is now' ...
     ' ns.prep.retrieve_bad_behavior_flags and will be removed in the future.']);
 
+varargout = cell(1, nargout);
 [varargout{:}] = ns.prep.retrieve_bad_behavior_flags(varargin{:});
 
 end
