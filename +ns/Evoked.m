6e12d141a3bc82e537e57b680348899a261299cf
Mert Özkan <mert.ozkan@rutgers.edu>
2025-10-13 13:41:48 -0400

    Evoked, skips empty group tables

--- +ns/Evoked.m
+++ +ns/Evoked.m
@@ -78,6 +78,10 @@ classdef Evoked < dj.Computed & dj.DJInstance
                 % fieldnames are the group label. Transform this to a cell
                 % vector {fieldname1, fieldvalue1,...}
                 groups = feval(evp_tpl.group_fun, eTbl, key);
+                if isempty(groups)
+                    fprintf("\t No clean epochs were found matching with the group description. Skipping...\n");
+                    return;
+                end
                 groups = namedargs2cell(groups);
 
             end
@@ -85,13 +89,13 @@ classdef Evoked < dj.Computed & dj.DJInstance
             % create a tpl per group
             n_gru = numel(groups)/2;
             evk_tpl = repmat(key, n_gru, 1);
-            evkch_tpl = {};
+            evkch_tpl = cell(1,n_gru);
 
             for iGru = 1:2:2*n_gru
 
                 t_fetch = tic;
                 gru_no = ceil(iGru/2);
-                fprintf("\tFetching signal for Group %d/%d and preparing submission...", gru_no, n_gru);
+                fprintf("\tFetching signal for Group %d/%d and preparing submission...\n", gru_no, n_gru);
                 if count(groups{iGru+1})==1;
                     fprintf("\t\t No clean epochs were found. Skipping the group...\n");
                     continue;
@@ -101,16 +105,20 @@ classdef Evoked < dj.Computed & dj.DJInstance
 
                 % create EvokedChannel tuple
                 ecTbl = ns.EpochChannel & groups{iGru+1} & ch_qry;
-                ec_tbl = fetchtable(ecTbl, 'signal');
-                toc(t_fetch);
-                % average by channel
-                [gru_idx, ch_id] = findgroups(ec_tbl.channel);
-
-                evkch_tpl{gru_no} = mergestruct(repmat(rmfield(evk_tpl(gru_no), 'n_epoch'), numel(ch_id), 1),...
-                    struct(channel = num2cell(ch_id), ...
-                    signal =  mat2cell( ...
-                    splitapply(@(x) mean(x,1, 'omitmissing'), ec_tbl.signal, gru_idx), ones(numel(ch_id),1) ...
-                    )));
+                if count(ecTbl)
+                    ec_tbl = fetchtable(ecTbl, 'signal');
+                    toc(t_fetch);
+                    % average by channel
+                    [gru_idx, ch_id] = findgroups(ec_tbl.channel);
+    
+                    evkch_tpl{gru_no} = mergestruct(repmat(rmfield(evk_tpl(gru_no), 'n_epoch'), numel(ch_id), 1),...
+                        struct(channel = num2cell(ch_id), ...
+                        signal =  mat2cell( ...
+                        splitapply(@(x) mean(x,1, 'omitmissing'), ec_tbl.signal, gru_idx), ones(numel(ch_id),1) ...
+                        )));
+                else
+                    fprintf("\t\t No clean epochs associated with the group were found.\n\n");
+                end
 
             end
 
