d4ec201b79e8cff6e4b410b2c0f62fb1215b497e
bartkrekelberg <bart@vision.rutgers.edu>
2025-10-13 11:49:27 +0200

    Added correction for boundary artifacts in maximin. Rewrote cascade to run on the full session and save results to file. Split off frame to experiment mapping.

---
+++ +sbx/framesForExperiment.m
@@ -0,0 +1,21 @@
+function [keepFrameIx,frameNsTime] = framesForExperiment(key)
+% Use the metadata added to the experiment by sbx.Preprocessed to match
+% frames to experiments
+exptT = ns.getMeta(ns.Experiment & key ,["nrframes" "nrplanes"]);
+exptT = sortrows(exptT,"starttime");
+exptT = convertvars(exptT,["nrframes" "nrplanes"],"double");
+row = find(key.starttime==exptT.starttime);
+cumFrames = cumsum(exptT.nrframes);
+if row>1
+    start = cumFrames(row-1);
+else
+    start = 0;
+end
+keepFrameIx =start + (1:exptT.nrframes(row));
+frameNsTime = get(ns.Experiment & key,'mdaq','prm','laserOnDigHigh','what',"clocktime");
+nrTTL = numel(frameNsTime);
+% There always appears to be 1 extraneous TTL; check the match with
+% this assumption
+assert((exptT.nrframes(row)+1)==floor(nrTTL/exptT.nrplanes(row)),'Cannot map SBX frames to trials; TTL-Frame mismatch (%d TTL %d frames in sbx).\n',nrTTL,exptT.nrframes(row));
+frameNsTime(1) =[];
+end
