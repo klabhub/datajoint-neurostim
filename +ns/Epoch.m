b1eb97363f934dc2c210819c16bbccc88dc5f59f
bartkrekelberg <bart@vision.rutgers.edu>
2025-10-29 17:23:47 +0100

    Created a data cache in the EpochChannel table.
    Converted code to use the cached data table
    Vectorized compute code.

--- +ns/Epoch.m
+++ +ns/Epoch.m
@@ -1,15 +1,21 @@
 %{
-# Preprocessed epoch data, the actual epochs are stored in ns.EpochChannel
+# Preprocessed epoch data, with epochs stored per trial and channel in the part table ns.EpochChannel
 -> ns.C             # The continuous data that were epoched
 -> ns.EpochParm     # Parameters used to epoch
 -> ns.Dimension     # Dimension that determines the conditions
 ---
 time : blob             # Time in milliseconds relative to the align event (which is defined in EpochParm) [start stop nrSamples]
-info : blob             # Info struct with information on preprocessing (.prep) and artifact removal (.art) done during epoching
+prep : blob             # Struct with information on preprocessing (.prep) done during epoching
+art   : blob            # Struct with information on artifact removal (.art) done during epoching
 %}
 classdef Epoch < dj.Computed & dj.DJInstance
+    properties (GetAccess =public,SetAccess = protected)
+        cache table
+        cacheQry (1,1) string =""
+    end
     properties (Dependent)
         time
+        samplingRate 
         keySource
     end
 
@@ -48,104 +54,16 @@ classdef Epoch < dj.Computed & dj.DJInstance
                 t= t{1};
             end
         end
-
-
-    end
-
-    methods (Access= public)
-
-        function plot (tbl,pv)
-            % Plot evoked responses for all rows in the table.
-            % Set the 'average' input to select which aspects to average
-            % over. By default, trials and channels are averaged. 
-            % 
-            arguments
-                tbl (1,1) ns.Epoch {mustHaveRows}
-                pv.delta (1,1) string = ""              % Show the difference using this named condition as the reference
-                pv.channel (:,1) double = []            % Select a subset of channels
-                pv.average (1,:) string {mustBeMember(pv.average,["starttime" "condition" "trial" "channel" "subject" "session_date"])} = ["trial" "channel"]  % Average over these dimensions
-                pv.tilesPerPage (1,1) double = 6        % Select how many tiles per page.
-            end
-            if isempty(pv.channel)
-                channelRestrict = "true"; % No restriction
-            else
-                channelRestrict = struct('channel',num2cell(pv.channel));
-            end
-            % Fetch the data
-            T =fetchtable(ns.Experiment*ns.Epoch*ns.EpochChannel*ns.EpochParm &  channelRestrict & proj(tbl),'paradigm','signal','time','condition','align','ORDER BY channel');
-            % Determine mean, ste and n averaging over the pv.average
-            % dimension (anything that is left out of grouping)
-            grouping = setdiff(["subject" "session_date" "starttime" "trial" "channel" "condition"  ],pv.average);
-            funs  = {@(x) mean(cat(2,x{:}),2,"omitmissing")', ...  % Mean
-                @(x) (std(cat(2,x{:}),0,2,"omitmissing")./sqrt(sum(~isnan(cat(2,x{:})),2,"omitmissing")))',...  % Standard error
-                @(x) sum(~isnan(cat(2,x{:})),2,"omitmissing")'};  % Non-Nan N
-            G = groupsummary(T, grouping, funs, 'signal');
-            G= renamevars(G,["fun1_signal" "fun2_signal" "fun3_signal"],["mean" "ste" "n"]);
-            % Combine with align/time/paradigm information. Note this
-            % assumes these are constant across the group (picking
-            % only the first or the unique here)
-            P = groupsummary(T, grouping, @(x) x(1), "align");
-            P = renamevars(P,"fun1_align","align");
-            G =innerjoin(G,P);
-            TM = groupsummary(T, grouping, @(x) unique(x)', ["time" "paradigm"]);
-            TM  = renamevars(TM,["fun1_time" "fun1_paradigm"],["time" "paradigm"]);                        
-            G =innerjoin(G,TM);
-            G= sortrows(G,intersect(["subject" "session_date" "starttime" "condition" "channel" "trial"],G.Properties.VariableNames,'stable'));
-
-            %% Figure
-            tileCntr=0;
-            nrTimeSeries = height(G);
-            for i = 1:nrTimeSeries
-                t =  G.time(i,:);
-                t = linspace(t(1),t(2),t(3));
-                align =G.align(i);
-                m = G.mean(i,:);
-                ste = G.ste(i,:);                
-                n = mean(G.n(i,:));
-                titlePV= setdiff(["paradigm" grouping],"condition");
-                ttlStr = strjoin(string(G{i,titlePV}),"/");
-                if i==1 || any(G{i,["subject" "session_date" "starttime"]} ~= G{i-1,["subject" "session_date" "starttime"]})
-                    % New  subject, session or experiment in a new tile
-                    if i>1
-                        % Add the legend string to the existing tile
-                        legend(h,legStr);
-                    end
-                    if mod(tileCntr,pv.tilesPerPage)==0
-                        figure;
-                    end
-                    % Start a new tile with empty handles
-                    nexttile;
-                    tileCntr =tileCntr+1;
-                    h = [];
-                    legStr = string([]);
-                    hold on
-                end
-                h = [h plot(t,m)];                %#ok<AGROW>
-                patch([t flip(t)],[m+ste flip(m-ste)],h(end).Color,FaceAlpha= 0.5);
-                legStr = [legStr G.condition(i)]; %#ok<AGROW>
-                title (ttlStr + " (n=" + string(n) +")",'Interpreter','none');
-                ylabel 'EP (\muV)'
-                xlabel (sprintf('Time after %s.%s (ms)',align.plugin,align.event));
-                % If delta is not empty, add the difference wave.
-                if pv.delta ~="" && G.condition(i) ~=pv.delta
-                    matchG = innerjoin(G,G(i,setdiff(grouping,"condition")));
-                    reference = find(matchG.condition ==pv.delta);
-                    if ~isempty(reference)
-                        y = m - matchG.mean(reference,:);
-                        ste = ste +matchG.ste(reference,:);
-                        h = [h plot(t,y)];                     %#ok<AGROW>
-                        patch([t flip(t)],[y+ste flip(y-ste)],h(end).Color,FaceAlpha= 0.5);
-                        legStr = [legStr G.condition(i)+"-"+ pv.delta]; %#ok<AGROW>
-                    end
-                end
-            end
-            legend(h,legStr); % For the last tile
-
+        
+        function v = get.samplingRate(tbl)
+           t = fetchn(tbl, 'time');
+           v= cellfun(@(x) x(3)./(x(2)-x(1)),t,'UniformOutput',true);           
         end
     end
+
+      
     methods (Access = protected)
         function makeTuples(tbl, key)
-
             %% Determine events to align to
             parmTpl = fetch(ns.EpochParm &key,'prep','art','align','window','channels');
             conditionTpl = fetch(ns.DimensionCondition&key,'trials');
@@ -219,7 +137,8 @@ classdef Epoch < dj.Computed & dj.DJInstance
             fprintf("Submitting to the server\n");
             epoch_tpl = mergestruct(key, ...
                 struct(time = [t(1) t(end) numel(t)],...
-                info = struct('prep',prepResults,'art',artResult)));
+                prep = prepResults,...
+                art =artResult));
             % Insert to Epoch table
             chunkedInsert(tbl, epoch_tpl);
 
